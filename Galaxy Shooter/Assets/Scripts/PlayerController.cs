b6b02dd3256495d329ccbf449d2b7ba6cbc41bae
PedroPassos87 <ph.passos@ges.inatel.br>
2024-08-24 21:03:07 -0300

    configuraçoes iniciais

---
+++ Galaxy Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        //take the current position = new position (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    void Update()
+    {
+        
+    }
+}
